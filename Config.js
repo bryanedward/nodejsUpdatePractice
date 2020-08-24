2b439ac712e2cd087f8300dd25bdcfb243899f31
eddev <edwardbrian96@gmail.com>
2020-08-23 23:00:23 -0500

    modific file .env

--- Config.js
+++ Config.js
@@ -1,13 +1,8 @@
 const dotenv = require('dotenv')
-const path = require('path')
 
-dotenv.config({
-    path: path.resolve(__dirname, process.env.NODE_ENV + '.env')
-})
+dotenv.config();
 
 module.exports = {
-    NODE_ENV: process.env.NODE_ENV || 'development',
-    HOST: process.env.HOST || '127.0.0.1',
-    PORT: process.env.PORT || 7000,
-    MONGODB_URI: process.env.MONGODB_URI ||'mongodb://localhost:27017/updatedb'
+    PORT: process.env.PORT,
+    MONGODB_URI: process.env.MONGODB_URI
 }
